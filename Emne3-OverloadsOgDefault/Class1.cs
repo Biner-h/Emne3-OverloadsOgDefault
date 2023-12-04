574dc82e181c7744948382365f1e251a0f3d1a4a
Biner Habib <Binerhabib@gmail.com>
2023-12-04 13:29:36 +0100

    Add project files.

---
+++ Emne3-OverloadsOgDefault/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emne3_OverloadsOgDefault
+{
+    internal class Class1
+    {
+        string Name { get; set; }
+        string Text { get; set; }
+
+        public Class1(string text, string name)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine(Text);
+        }
+
+        public void printInfo(string extraInfo)
+        {
+            Console.WriteLine(Text + extraInfo);
+        }
+
+
+    }
+}
---
+++ Emne3-OverloadsOgDefault/Program.cs
@@ -0,0 +1,8 @@
+﻿using Emne3_OverloadsOgDefault;
+
+static void printWelcomeMessage()
+{
+    var person = new Class1("Hei og velkommen", "Terje");
+    person.printInfo(" Terje");
+}
+printWelcomeMessage();
